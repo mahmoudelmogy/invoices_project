9d222205057e4fe195dd0dde82472e92a55ef20d
mahmoud elmogy <melmogy60@gmail.com>
2023-07-15 11:45:20 +0300

    first commit

---
+++ app/Http/Controllers/InvoicesDetailsController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\invoices_details;
+use Illuminate\Http\Request;
+use App\models\invoices;
+use App\models\invoices_attachments;
+use Illuminate\Support\Facades\Storage;
+use File;
+
+class InvoicesDetailsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\invoices_details  $invoices_details
+     * @return \Illuminate\Http\Response
+     */
+    public function show(invoices_details $invoices_details)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\invoices_details  $invoices_details
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $invoices = invoices::where('id',$id)->first();
+        $details  = invoices_Details::where('id_Invoice',$id)->get();
+        $attachments  = invoices_attachments::where('invoice_id',$id)->get();
+
+        return view('invoices.details_invoice',compact('invoices','details','attachments'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\invoices_details  $invoices_details
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, invoices_details $invoices_details)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\invoices_details  $invoices_details
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request)
+    {
+        $invoices = invoices_attachments::findOrFail($request->id_file);
+        $invoices->delete();
+        Storage::delete($request->invoice_number.'/'.$request->file_name);
+       // Storage::disk('public_uploads')->delete($request->invoice_number.'/'.$request->file_name);
+        session()->flash('delete', 'تم حذف المرفق بنجاح');
+        return back();
+    }
+    public function open_file($invoice_number,$file_name)
+
+    {
+        $files = Storage::disk('public_uploads')->getDriver()->getAdapter()->applyPathPrefix($invoice_number.'/'.$file_name);
+        return response()->file($files);
+    }
+    public function get_file($invoice_number,$file_name)
+
+    {
+        $contents= Storage::disk('public_uploads')->getDriver()->getAdapter()->applyPathPrefix($invoice_number.'/'.$file_name);
+        return response()->download( $contents);
+    }
+}
---
+++ database/seeders/CreateAdminUserSeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Database\Seeders;
+
+//use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\models\User;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+
+class CreateAdminUserSeeder extends Seeder
+{
+/**
+* Run the database seeds.
+*
+* @return void
+*/
+public function run()
+{
+    
+                // Create the 'owner' role
+                $user = User::create([
+                    'name' => 'Mahmoud.Elmogy', 
+                    'email' => 'melmogy60@gmail.com',
+                    'password' => bcrypt('123456'),
+                    'roles_name' => ["owner"],
+                    'Status' => 'مفعل',
+                    ]);
+              
+                    $role = Role::create(['name' => 'owner']);
+               
+                    $permissions = Permission::pluck('id','id')->all();
+              
+                    $role->syncPermissions($permissions);
+               
+                    $user->assignRole([$role->id]);
+            
+            
+            
+    
+
+
+
+
+}
+}
